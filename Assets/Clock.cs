81f89559e2d83a3c14a42a21e21dba4d572a19c7
TarCode <tarcode33@gmail.com>
2018-09-26 19:29:14 +0200

    added continuous and discrete update with toggle to clock script

--- Assets/Clock.cs
+++ Assets/Clock.cs
@@ -7,9 +7,34 @@ public class Clock : MonoBehaviour {
         degreesPerMinute = 6f,
         degreesPerSecond = 6f;
 
+    public bool continuous;
+
     public Transform hoursTransform, minutesTransform, secondsTransform;
 
     void Update()
+    {
+        if (continuous)
+        {
+            UpdateContinuous();
+        }
+        else
+        {
+            UpdateDiscrete();
+        }
+    }
+
+    void UpdateContinuous()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
+        secondsTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerSecond, 0f);
+    }
+
+    void UpdateDiscrete()
     {
         DateTime time = DateTime.Now;
         hoursTransform.localRotation =
